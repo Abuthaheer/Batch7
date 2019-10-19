390099fae1a9abf7a16800af4ecca74306386223
Sheela <sheilacse@gmail.com>
2019-10-19 10:29:21 +0530

    Added a updated file - Abu

---
+++ src/test/java/testScripts/AbuUpdated.java
@@ -0,0 +1,33 @@
+package testScripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.LoginObject;
+
+public class LoginTests {
+
+	LoginObject lg;
+
+	public LoginTests() {
+		lg = new LoginObject();
+	}
+	
+	
+	@Test
+	public void verifyLogin(){
+
+				lg.login("Abuthaheer123", "PE64C9");
+
+
+		lg.login("Abuthaheer123", "PE4C9");
+		lg.login("Abuthaheer", "PE64C9");
+		lg.login("Abuthaheer", "PE64C9");
+		lg.login("Abuthaheer123", "PE64C9");
+
+		Assert.assertTrue(lg.getTitlte().contains("Search"), 
+				"Failed: Login not successfull");
+	}
+	
+
+}
